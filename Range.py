20934c006026c26068a21fcf3bf6e583e6a1b102
SabrinaMannan <47242360+SabrinaMannan@users.noreply.github.com>
2020-04-07 20:52:38 +0600

    Create Range.py

---
+++ Range.py
@@ -0,0 +1,13 @@
+#range upto which u want to print
+num = list(range(5))
+print(num)
+print(num[3]) #printing the number in index 3
+
+#range with starting and ending point
+num = list(range(0,11))
+print(num)
+
+#range with starting,ending point and interval
+num = list(range(1,20,2))
+
+print(num)
