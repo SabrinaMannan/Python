ccb5e994bfdc78960e8e7170a0c34e23ca41c434
SabrinaMannan <47242360+SabrinaMannan@users.noreply.github.com>
2020-04-07 20:47:18 +0600

    Create Formatted string.py

---
+++ Formatted string.py
@@ -0,0 +1,7 @@
+num1 = 20
+num2 = 30
+
+print(f"{num1}+{num2} = {num1+num2}") #f is printing the value of num1 and 2
+
+print("The code",end=" ") #end= is printing the next line"ends here" in the same line
+print("ends here.")
