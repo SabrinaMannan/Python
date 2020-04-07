e207bd973a342be3686a8086f347541efb5238b7
SabrinaMannan <47242360+SabrinaMannan@users.noreply.github.com>
2020-04-07 20:51:06 +0600

    Create CheckingMarks.py

---
+++ CheckingMarks.py
@@ -0,0 +1,9 @@
+marks = int(input("Enter your marks:"))
+
+if marks>=60:
+    print("PASS")
+    print("CONGRATULATIONS!!")
+
+else:
+    print("Better luck next time")
+    print("Don't be hopeless and try hard again")
