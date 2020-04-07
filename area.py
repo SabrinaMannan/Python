b2fc449de14a28f3e9940045d2911983a06296d9
SabrinaMannan <47242360+SabrinaMannan@users.noreply.github.com>
2020-04-07 20:41:58 +0600

    Create area.py

---
+++ area.py
@@ -0,0 +1,9 @@
+base = float(input("Base:"))
+height = float(input("height:"))
+
+area = 0.5 *base*height
+print("Area of the triangle is",area,"square unit")
+
+radius = float(input("Radius ="))
+area = 3.14 *radius* radius
+print("Area of the circle is",area)
