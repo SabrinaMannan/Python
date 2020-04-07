502ea401bcf78f4b4762fb83a81af8605c362fd5
SabrinaMannan <47242360+SabrinaMannan@users.noreply.github.com>
2020-04-07 20:41:17 +0600

    Create SumOfNumber.py

---
+++ SumOfNumber.py
@@ -0,0 +1,7 @@
+n = int(input("Enter the no. upto which you want the sum:"))
+sum = 0
+i = int(input("from where u want to  start adding:"))
+while i<=n:
+    sum = sum+i
+    i= i+1
+print("Sum is",sum)
