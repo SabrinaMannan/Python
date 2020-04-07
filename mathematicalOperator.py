5d944b74f7af6ca4e411a065661c3856acf27505
SabrinaMannan <47242360+SabrinaMannan@users.noreply.github.com>
2020-04-07 20:52:07 +0600

    Create mathematicalOperator.py

---
+++ mathematicalOperator.py
@@ -0,0 +1,16 @@
+num1=int(input("Enter a number:"))
+num2=int(input("Enter a number:"))
+
+result = num1 + num2
+print("The result is",result)
+result = num1 - num2
+print("The result is",result)
+result = num1 * num2
+print("The result is",result)
+
+
+#using functions
+from math import *
+
+print(floor(4.54)) #prints the prev lowest integar
+print(ceil(6.54)) #prints the next lowest integar
