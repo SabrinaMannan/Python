88562610677ff392989c43adcf551d29cb838d56
SabrinaMannan <47242360+SabrinaMannan@users.noreply.github.com>
2020-04-07 20:53:10 +0600

    Create Series.py

---
+++ Series.py
@@ -0,0 +1,63 @@
+#1+2+3+....+n
+"""
+n = int(input("Enter the last number:"))
+sum = 0
+for x in range(1, n+1,1):
+    sum= sum + x
+
+print(sum)
+"""
+#2+4+....n Sum of even numbers
+"""
+n= int(input("Enter the last number:"))
+sum = 0
+for x in range(2,n+1,2):
+    sum = sum+x
+
+print(sum)
+"""
+"""
+#1+3+5..... sum of odd number
+n = int(input("Enter the last number:"))
+sum = 0
+for x in range(1,n+1,2):
+    sum = sum + x
+
+print(sum)
+"""
+"""
+#4+8+12...
+n = int(input("Enter the last number:"))
+sum = 0
+for x in range(0,n+1,4):
+    sum = sum + x
+
+print(sum)
+"""
+"""
+#1*1+2*2+.....+n*n
+n = int(input("Enter the last number:"))
+sum = 0
+for x in range(0,n+1,1):
+    sum = sum + x*x
+
+print(sum)
+"""
+"""
+#2*2+4*4+....
+
+n = int(input("Enter the last number:"))
+sum = 0
+for x in range(2,n+1,2):
+    sum = sum+x*x
+print(sum)
+"""
+"""
+#1+1/2+1/3+....+1/n
+n = int(input("Enter the last number:"))
+sum = 0
+for x in range(1,n,1):
+    sum = sum + 1/x
+print(sum)
+"""
+
